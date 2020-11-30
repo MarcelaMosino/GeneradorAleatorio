7f931add0d7c097b0c92668a285e2ee9ccbfdb84
DianaRamirez20 <73078753+DianaRamirez20@users.noreply.github.com>
2020-11-30 08:18:56 -0600

    Update Generador.java

--- Generador.java
+++ Generador.java
@@ -1,31 +1,38 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Clase Generadora
  */
+
+//Importamos la clase io
 import java.io.*;
 
 public class Generador {
+    //Creamos el objeto Validación
     static Validacion v = new Validacion();
+    //Creamos las variables
     private long m, h, d, a;
+    ///Creamos un arreglo
     private long[] numeros;
 
+    //Método de Bienvenida!
     public void inicio(){
         String c= "El siguiente programa genera numeros aleatorios\n";
         c += "utilizando el método multiplicativo decimal(MMD)\n";
         c += "y los guarda en un archivo de texto";
         v.msg(c);
     }
-
+    
+    //Generar numeros aleatorios
     public void calculaD(int n){
         if(n<=500)
             d=4;
         else
             d = Math.round(1+Math.log10(n*2)+.9999999);
     }
-
+    //Calculando los números
     public void calculaHMA(){
+        
         int t = v.obtenT();
+      
         int p = v.obtenP();
         h = Math.round(5*Math.pow(10, d-2));
         m = Math.round(Math.pow(10,d));
@@ -34,7 +41,7 @@ public class Generador {
         else
             a = Math.abs((200*t)-p);
     }
-
+     //Obtenemos los números impares
     public long obtenSemilla(){
         long valor=0;
 
@@ -43,7 +50,7 @@ public class Generador {
         } while((valor%5)==0);
         return valor;
     }
-
+    //Recorre el arreglo
     public long calculaNumero(){
         long n0 =obtenSemilla();
         numeros = new long[(int)h];
@@ -53,12 +60,12 @@ public class Generador {
             numeros[i]= (numeros[i-1]*a)%m;
         return 0;
     }
-
+    //Método main
     public static void main(String[] args)throws IOException{
         Generador g =new Generador();
         int r;
         int n;
-
+        //Menú donde se llaman todos los métodos y lo imprime en el archivo
         g.inicio();
         do{
             n = v.capInt("Periodo mínimo a generar");
